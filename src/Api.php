b63cf9cde37949a17a95d9c9f6e5df6e194d336d
Weifeng Peng <pwf0112@163.com>
2016-10-15 18:07:27 +0800

    n

--- src/Api.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Corp;
-
-interface Api
-{
-	const ACCESS_TOKEN = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s';
-	const USER_INFO = 'https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=%s&code=%s';
-	const OAUTH = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=SCOPE&state=%s#wechat_redirect';
-}
